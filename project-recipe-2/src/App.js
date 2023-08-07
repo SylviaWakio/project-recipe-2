0b68feb6064336f6e269b0d37895e56fedd48dd8
sylvianjeru <sylvianjeru63@gmail.com>
2023-08-07 04:01:14 +0300

    done

--- project-recipe-2/src/App.js
+++ project-recipe-2/src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { React } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/layouts/Header";
+import Register from "./components/accounts/Register";
+import Login from "./components/accounts/Login";
+import Landing from "./components/layouts/Landing";
+import Recipes from "./components/recipe/Recipes";
+import RecipeDetail from "./components/recipe/RecipeDetail";
+import RecipeCreate from "./components/recipe/RecipeCreate";
+import RecipeEdit from "./components/recipe/RecipeEdit";
+import WithPrivateRoute from "./utils/WithPrivateRoute";
+import Dashboard from "./components/layouts/Dashboard";
 
-function App() {
+import Profile from "./components/accounts/Profile";
+import MyRecipes from "./components/recipe/MyRecipes";
+import SavedRecipes from "./components/recipe/SavedRecipes";
+
+import ErrorDiv from "./components/layouts/ErrorDiv";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <Header />
+      <ErrorDiv />
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+
+        <Route exact path="/recipe" element={<Recipes />} />
+        <Route
+          exact
+          path="/recipe/:id"
+          element={
+            <WithPrivateRoute>
+              <RecipeDetail />
+            </WithPrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/recipe/create"
+          element={
+            <WithPrivateRoute>
+              <RecipeCreate />
+            </WithPrivateRoute>
+          }
+        />
+        <Route
+          exact
+          path="/recipe/:id/edit"
+          element={
+            <WithPrivateRoute>
+              <RecipeEdit />
+            </WithPrivateRoute>
+          }
+        />
+
+        <Route
+          path="dashboard"
+          element={
+            <WithPrivateRoute>
+              <Dashboard />
+            </WithPrivateRoute>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route path="profile" element={<Profile />} />
+          <Route path="myRecipes" element={<MyRecipes />} />
+          <Route path="savedRecipes" element={<SavedRecipes />} />
+        </Route>
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
